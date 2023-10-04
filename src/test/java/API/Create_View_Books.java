4fed58b061e5145b00781da500e9fcd83c23207d
ale.vasilache <ale.vasilache@gmail.com>
2023-10-04 19:57:58 +0300

    Initial commit

---
+++ src/test/java/API/Create_View_Books.java
@@ -0,0 +1,4 @@
+package API;
+
+public class Create_View_Books {
+}
---
+++ src/test/java/Teste/ApelareMetodeDeTest.java
@@ -0,0 +1,29 @@
+package Teste;
+
+import org.testng.annotations.Test;
+
+public class ApelareMetodeDeTest {
+
+
+    @Test
+    public void LendBookTest(){
+
+    }
+
+    @Test
+    public void CreateEmptyBook(){
+
+    }
+
+    @Test
+    public void DeleteBook(){
+
+    }
+    
+
+    @Test
+    public void EditBookTest(){
+
+    }
+
+}
